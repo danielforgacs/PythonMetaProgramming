57837148a1b8e381027cca84342c9939ef952ac5
ford@AcerS1 <forgacs.daniel@gmail.com>
2019-01-13 00:57:35 +0000

    added nodetransformer example

---
+++ node_transformer_example_01.py
@@ -0,0 +1,55 @@
+import ast
+
+
+class MyTransformer(ast.NodeTransformer):
+    def visit_Str(self, node):
+        return ast.Str(node.s.upper())
+
+    def visit_Num(self, node):
+        node.n = 123
+        return node
+
+    def visit_FunctionDef(self, node):
+        node = self.generic_visit(node)
+        node.name = 'HeyFunc'
+        node = ast.FunctionDef(
+            name='blabla',
+            args=ast.arguments(
+                args=[],
+                vararg=None,
+                kwonlyargs=[],
+                kw_defaults=[],
+                kwarg=None,
+                defaults=[]),
+            body=node.body,
+            decorator_list=[],
+        )
+        return node
+
+node = ast.parse("""
+def bad_func_name():
+    print('this func called.')
+    return 5
+""")
+
+MyTransformer().visit(node)
+
+ast.fix_missing_locations(node)
+
+comp = compile(node, '', 'exec')
+exec(comp)
+
+try:
+    print('bad_func_name()', bad_func_name())
+except:
+    pass
+
+try:
+    print('HeyFunc()', HeyFunc())
+except:
+    pass
+
+try:
+    print('blabla()', blabla())
+except:
+    pass
