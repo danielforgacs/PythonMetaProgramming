2a96171ce7d367e3d230d18611e16d5463f94f78
ford@acer1 <-@->
2019-09-18 18:54:59 +0100

    added docs

--- ASTSwapImport/importer.py
+++ ASTSwapImport/importer.py
@@ -1,3 +1,15 @@
+"""
+this module shows how to replace an import in
+a module dynamically. This module "imports"
+the "module" module, replaces the "math"
+import with "mockmath" and runs it.
+
+For the imported and tweaked module to
+be available here as an actual module,
+the AST is executed in a new dynamically
+created module's namespace.
+"""
+
 import os
 import sys
 import ast
@@ -14,21 +26,38 @@ sys.path += [ROOT_PARENT_DIR]
 
 
 with open(MODULEFILE, 'r') as filehandler:
+    # The module is read as text for the update.
     sourcecode = filehandler.read()
 
+
+# The imported module's code is parsed to an AST
 source_ast = ast.parse(sourcecode)
+
+# A new replacement AST node is created
+# with the mock modules names.
 newimport = ast.Import()
-newimport.names = [ast.alias(name='mockmath', asname='math')]
+newimport.names = [
+    ast.alias(name='mockmath', asname='math')
+]
 
 
 class ImportSwapper(ast.NodeTransformer):
+    """
+    This nodetransformer replaces the
+    "math" import node with the new
+    import node that imports "mockmath"
+    """
     def visit_Import(self, node):
         ast.copy_location(newimport, node)
         return newimport
 
 
 tweakedsource_ast = ImportSwapper().visit(source_ast)
+
+# The tweaked AST is compiled
 compiled = compile(tweakedsource_ast, 'tweakedmodule', 'exec')
+
+
 tempnamespace = {}
 
 exec(compiled, tempnamespace)
