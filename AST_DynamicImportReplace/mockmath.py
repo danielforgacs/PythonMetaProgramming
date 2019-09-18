97a1bfd9b18d63d5b3277f4f34b4bba3d7a65f09
ford@acer1 <-@->
2019-09-18 19:29:11 +0100

    fixed module after reorg.

--- AST_DynamicImportReplace/mockmath.py
+++ AST_DynamicImportReplace/mockmath.py
@@ -1,7 +1,7 @@
 """
 this is the mock math module we want to use
 for testing. this module is used in the
-"module" module. When the importer module
+"mathutils" module. When the test module
 is run, it replaces the "math" import
 with this module.
 """
--- AST_DynamicImportReplace/test.py
+++ AST_DynamicImportReplace/test.py
@@ -1,13 +1,13 @@
 """
 this module shows how to replace an import in
 a module dynamically. This module "imports"
-the "module" module, replaces the "math"
-import with "mockmath" and runs it.
+the "mathutils" module, replaces the "math"
+import in it with "mockmath" and runs it.
 
 For the imported and tweaked module to
-be available here as an actual module,
-the AST is executed in a new dynamically
-created module's namespace.
+be available here as an actual module with
+it's own namespace, the AST is executed
+in a new dynamically created module's namespace.
 """
 
 import os
@@ -20,13 +20,18 @@ ROOT_DIR = (
         os.path.abspath(__file__)
 ))
 ROOT_PARENT_DIR = os.path.dirname(ROOT_DIR)
-MODULEFILE = os.path.join(ROOT_PARENT_DIR, 'ASTSwapImport/module.py')
 
 sys.path += [ROOT_PARENT_DIR]
 
 
+DIR_NAME = os.path.basename(ROOT_DIR)
+MODULEFILE = os.path.join(ROOT_PARENT_DIR, ROOT_DIR, 'mathutils.py')
+
+assert os.path.isfile(MODULEFILE)
+
+
+# The module is read as text for the update.
 with open(MODULEFILE, 'r') as filehandler:
-    # The module is read as text for the update.
     sourcecode = filehandler.read()
 
 
@@ -45,7 +50,10 @@ class ImportSwapper(ast.NodeTransformer):
     """
     This nodetransformer replaces the
     "math" import node with the new
-    import node that imports "mockmath"
+    import node that imports "mockmath".
+
+    The new "mockmath" module is
+    imported as "math"
     """
     def visit_Import(self, node):
         ast.copy_location(newimport, node)
@@ -55,7 +63,7 @@ class ImportSwapper(ast.NodeTransformer):
 tweakedsource_ast = ImportSwapper().visit(source_ast)
 
 # The tweaked AST is compiled
-compiled = compile(tweakedsource_ast, 'tweakedmodule', 'exec')
+compiled = compile(tweakedsource_ast, '<MODULE>', 'exec')
 
 # A new module is created to act as the
 # namespace for the executed code
@@ -73,4 +81,4 @@ print()
 print('[tweakedmodule]:', tweakedmodule)
 print('[tweakedmodule.__name__]:', tweakedmodule.__name__)
 print('[tweakedmodule.__file__]:', tweakedmodule.__file__)
-print('[tweakedmodule.math]:', tweakedmodule.math)
+print('[*tweakedmodule.math]:', tweakedmodule.math)
