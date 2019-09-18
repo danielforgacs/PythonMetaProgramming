5fe9cd4953e14cee5e57e9f4a0dc6f434b8a91a9
ford@acer1 <-@->
2019-09-18 18:47:18 +0100

    added docs

--- ASTSwapImport/mockmath.py
+++ ASTSwapImport/mockmath.py
@@ -1,1 +1,8 @@
+"""
+this is the mock math module we want to use
+for testing. this module is used in the
+"module" module. When the importer module
+is run, it replaces the "math" import
+with this module.
+"""
 print('>>> MOCK MATH IMPORTED')
