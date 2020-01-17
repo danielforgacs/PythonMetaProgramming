3d8db623a8f4fce5614ceb427ae23037fd2a9fd0
danielf@AcerS1 <-@->
2020-01-17 21:49:55 +0000

    __wip__

--- DecoratorFuncChange/helloworld.py
+++ DecoratorFuncChange/helloworld.py
@@ -5,6 +5,16 @@ import dis
 def fixer(func):
     def wrapper():
         co = func.__code__
+        # print(dir(co))
+        # print(co.co_code)
+        # print(co.co_consts)
+
+        # co.co_consts = (None, 'LH')
+        # dis.dis(co)
+        # func.__code__.co_contsts = ('1', '2')
+        # dis.dis(co.co_code)
+        # ast.NodeVisitor().visit(func)
+        # ast.NodeVisitor().visit(co)
         return func()
     return wrapper
 
@@ -17,3 +27,4 @@ def hello_world():
 
 
 hello_world()
+
