57d7dc0b1206eb59232a738b6bc123624078212a
danielf@AcerS1 <-@->
2020-02-18 13:53:40 +0000

    update

--- PrototypeObjectExplained/protoobj.py
+++ PrototypeObjectExplained/protoobj.py
@@ -1,10 +1,12 @@
 class CustomObj:
     def __init__(self, idx, title):
+        print('::[__init__]')
         self.idx = idx
         self.title = title
         self.sameattr = 'sameattr'
 
     def __repr__(self):
+        print('::[__repr__]')
         suprep = super().__repr__()
         return '<{}{}>'.format(self.idx, suprep)
 
@@ -12,18 +14,21 @@ class CustomObj:
         """
         called by: len()
         """
+        print('::[__len__]')
         return 12345
 
     def __eq__(self, other):
         """
         called by: "=="
         """
+        print('::[__eq__]')
         return self.sameattr == other.sameattr
 
     def __lt__(self, other):
         """
         less than, called by: "<"
         """
+        print('::[__lt__]')
         return self.idx < other.idx
 
 
@@ -32,8 +37,28 @@ class CustomObj:
         called before __init__. Can be used to create
         singletons.
         """
+        print('::[__new__]')
         instance = super().__new__(cls)
         return instance
+
+
+    def __getattribute__(self, attr):
+        """
+        1st call on attribute access.
+        """
+        print('::[__getattribute__]')
+        value = super().__getattribute__(attr)
+        return value
+
+
+    def __getattr__(self, attr):
+        """
+        2nd call on attribute access.
+        called when attr is missing.
+        """
+        print('::[__getattr__]')
+        value = 'generated value'
+        return value
 
 
 
@@ -41,25 +66,20 @@ class CustomObj:
 if __name__ == '__main__':
     pass
 
+    print(':: help on object:')
+    print(help(object))
+    print(':: --------------:')
+
     obj1 = CustomObj(idx=1, title='one')
     obj2 = CustomObj(idx=2, title='two')
 
     objs = [obj1, obj2]
 
-    print(':: help on object:')
-    print(help(object))
-
-    print('\n:: print obj')
     print(obj1)
-
-    print('\n:: print objs')
     print(objs)
-
-    print('\n:: len()')
     print(len(obj1))
-
-    print('\n:: ==')
     print(obj1 == obj2)
-
-    print('\n:: <')
     print(obj1 < obj2)
+    print(getattr(obj1, 'idx'))
+    print(obj1.title)
+    print(obj1.missing)
