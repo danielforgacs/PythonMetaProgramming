99e05e279e8283bb0628808a406d690d95357b18
danielf@AcerS1 <-@->
2020-02-21 23:11:47 +0000

    refactor

--- PrototypeObjectExplained/protoobj.py
+++ PrototypeObjectExplained/protoobj.py
@@ -1,66 +1,76 @@
+def print_methodinfo(method):
+    def wrapper(*args, **kwargs):
+        print('[CALL] {}'.format(method.__name__))
+        result = method(*args, **kwargs)
+        return result
+    return wrapper
+
+
+
 class CustomObj:
+    @print_methodinfo
     def __init__(self, idx, title):
-        print('::[__init__]')
         self.idx = idx
         self.title = title
         self.sameattr = 'sameattr'
 
+    @print_methodinfo
     def __repr__(self):
-        print('::[__repr__]')
         suprep = super().__repr__()
         return '<{}{}>'.format(self.idx, suprep)
 
+    @print_methodinfo
     def __len__(self):
         """
         called by: len()
         """
-        print('::[__len__]')
         return 12345
 
+    @print_methodinfo
     def __eq__(self, other):
         """
         called by: "=="
         """
-        print('::[__eq__]')
         return self.sameattr == other.sameattr
 
+    @print_methodinfo
     def __lt__(self, other):
         """
         less than, called by: "<"
         """
-        print('::[__lt__]')
         return self.idx < other.idx
 
 
+    @print_methodinfo
     def __new__(cls, *args, **kwargs):
         """
         called before __init__. Can be used to create
         singletons.
         """
-        print('::[__new__]')
         instance = super().__new__(cls)
         return instance
 
 
+    @print_methodinfo
     def __getattribute__(self, attr):
         """
         1st call on attribute access.
         """
-        print('::[__getattribute__]')
         value = super().__getattribute__(attr)
         return value
 
 
+    @print_methodinfo
     def __getattr__(self, attr):
         """
         2nd call on attribute access.
         called when attr is missing.
         """
-        print('::[__getattr__]')
         value = 'generated value'
         return value
 
 
+    @print_methodinfo
     def __contains__(self, value):
         """
         called by: "in"
@@ -73,21 +83,18 @@ class CustomObj:
 if __name__ == '__main__':
     pass
 
-    print(':: help on object:')
-    print(help(object))
-    print(':: --------------:')
-
     obj1 = CustomObj(idx=1, title='one')
     obj2 = CustomObj(idx=2, title='two')
 
     objs = [obj1, obj2]
 
-    print(obj1)
-    print(objs)
-    print(len(obj1))
-    print(obj1 == obj2)
-    print(obj1 < obj2)
-    print(getattr(obj1, 'idx'))
-    print(obj1.title)
-    print(obj1.missing)
-    print('a' in obj1)
+    len(obj1)
+    obj1 == obj2
+    obj1 < obj2
+    getattr(obj1, 'idx')
+    obj1.title
+    obj1.missing
+    'a' in obj1
+
+    print('obj1: ', obj1)
+    print('objs: ', objs)
